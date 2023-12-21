fe10de4fc50a7f66ef21adfeb680c2cc4d9fa83e
suhaasbadada <suhaasbadada@gmail.com>
2023-12-21 16:54:18 +0530

    complete all models

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,34 +1,35 @@
 package database
 
-import(
+import (
 	"context"
 	"fmt"
 	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBinstance() *mongo.Client {
-    MongoDbURI := "mongodb://localhost:27017"
-    fmt.Println(MongoDbURI)
+	MongoDbURI := "mongodb://localhost:27017"
+	fmt.Println(MongoDbURI)
 
-    // Use mongo.Connect directly with a context and client options
-    client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(MongoDbURI))
-    handleError(err)
+	// Use mongo.Connect directly with a context and client options
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(MongoDbURI))
+	handleError(err)
 
-    fmt.Println("Connected to MongoDB!")
-    return client
+	fmt.Println("Connected to MongoDB!")
+	return client
 }
 
-func handleError(err error){
-	if err!=nil{
+func handleError(err error) {
+	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-var client *mongo.Client=DBinstance()
+var Client *mongo.Client = DBinstance()
 
-func OpenCollection(client *mongo.Client,collectionName string) *mongo.Collection{
-	var collection *mongo.Collection=client.Database("restaurant").Collection(collectionName)
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
 	return collection
 }
